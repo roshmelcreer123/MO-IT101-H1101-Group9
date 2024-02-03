69c74f7d391d8e26e5774e0025ab8bb52713dcd0
roshmelcreer123 <lr.rcreer@mmdc.mcl.edu.ph>
2024-02-03 15:46:38 +0800

    Add Eclipse project files and compiled binaries

---
+++ src/motor_ph/TestCode.java
@@ -0,0 +1,13 @@
+package motor_ph;
+
+public class TestCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+		System.out.println("Roshmel Creer");
+		System.out.println("Roshmel\nCreer");
+	}
+
+}
